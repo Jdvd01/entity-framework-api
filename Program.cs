e035381477f65fa25f64230a7fea6e26e9d9df4f
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-06-03 19:52:43 -0400

    ✅ Creates an endpoint to test the API

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using efCourse;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure dbcontext with in-memory database
+// This is useful for testing and development purposes
+builder.Services.AddDbContext<TasksContext>(opt => opt.UseInMemoryDatabase("TasksDb"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconection", async ([FromServices] TasksContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    string dbStatus = dbContext.Database.IsInMemory() ? "In-Memory Database" : "Not In-Memory";
+    return Results.Ok($"Database connection status: {dbStatus}");
+});
 
 app.Run();
