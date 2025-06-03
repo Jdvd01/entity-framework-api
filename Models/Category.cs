d88cc33aac1981e5b7fcb593f7db1506614b88ce
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-06-03 17:24:34 -0400

    🎨 Improve models with Data Annotations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace efCourse.Models;
 
 public class Category
 {
+    [Key]
     public Guid CategoryId { get; set; }
+
+    [Required]
+    [MaxLength(50)]
     public string Name { get; set; }
+
     public string Description { get; set; }
 
     // relationship
--- Models/Task.cs
+++ Models/Task.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace efCourse.Models;
 
 public class Task
 {
+    [Key]
     public Guid TaskId { get; set; }
+
+    [ForeignKey("CategoryId")]
     public Guid CategoryId { get; set; }
+
+    [Required]
+    [MaxLength(200)]
     public string Title { get; set; }
     public string Description { get; set; }
     public Priority TaskPriority { get; set; }
@@ -11,6 +20,11 @@ public class Task
 
     // relationship
     public virtual Category Category { get; set; }
+
+    // This property is not mapped to the database
+    // It can be used in code but will not be persisted
+    [NotMapped]
+    public string Summary { get; set; }
 }
 
 public enum Priority
