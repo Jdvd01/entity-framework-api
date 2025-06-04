27653333462038b8bebcd48e7a429f48a30c6758
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-06-03 20:56:17 -0400

    ✏️ Fix endpoint typo

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/dbconection", async ([FromServices] TasksContext dbContext) =>
+app.MapGet("/dbconnection", async ([FromServices] TasksContext dbContext) =>
 {
     dbContext.Database.EnsureCreated();
     string dbStatus = dbContext.Database.IsInMemory() ? "In-Memory Database" : "Not In-Memory";
